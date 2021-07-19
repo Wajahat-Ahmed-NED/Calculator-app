309a2362367197b13dc1afcb8b2557fc71745eaa
Wajahat <wajahatalimay2001@gmail.com>
2021-07-19 05:57:56 -0700

    calculator

---
+++ app.js
@@ -0,0 +1,15 @@
+function getValue() {
+   
+    var a = document.getElementById("inp")
+}
+    function cal(num){
+    console.log(num)
+
+    a.value += num
+}
+
+function result(){
+    a.value = eval(a.value)
+}
+
+
